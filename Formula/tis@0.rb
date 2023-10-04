57cba4748a5874c6be9d4c06956f98b0977467f2
tetrate-ci <52504619+tetrate-ci@users.noreply.github.com>
2023-10-04 01:47:06 +0000

    Apply automatic changes

--- Formula/tis@0.rb
+++ Formula/tis@0.rb
@@ -2,14 +2,14 @@
 # frozen_string_literal: true
 
 class TisAT0 < Formula
-  desc "A CLI for TIS API (v0.0.1-nightly+c120061)"
+  desc "A CLI for TIS API (v0.0.1-nightly+e6160be)"
   homepage "https://tis.tetrate.io"
-  version "0.0.1-n1696382995419" # v0.0.1-nightly+c120061
+  version "0.0.1-n1696384026152" # v0.0.1-nightly+e6160be
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/tis-system/artifacts/releases/download/nightly/cli_nightly_darwin_amd64.tar.gz"
-      sha256 "cf8cece76c40441a69791bfe9e2f19980e7c0c8688135fb00ac6e287d7ee4856"
+      sha256 "e4b001b6c932938919cab1d4dc2e661614b228010b347d5bb61d98994d4ba2f2"
 
       def install
         bin.install "tis"
@@ -17,7 +17,7 @@ class TisAT0 < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/tis-system/artifacts/releases/download/nightly/cli_nightly_darwin_arm64.tar.gz"
-      sha256 "225e1cbab1bbbfd38bdb675fac5621c4b8e63f2054f684eaa778275639a10318"
+      sha256 "f4dd8905d34ef2dd88460ec770db50e7f52dd9c0da93a3f24f185f352ee2037c"
 
       def install
         bin.install "tis"
@@ -28,7 +28,7 @@ class TisAT0 < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/tis-system/artifacts/releases/download/nightly/cli_nightly_linux_amd64.tar.gz"
-      sha256 "f07154b201eb632caddf49590adb7b19f8e061b0531bc0e809118c621f1fbcae"
+      sha256 "06f6903e9e367f2634bb805e3332341f9b240840e2ef5678b183c14a4cb989ad"
 
       def install
         bin.install "tis"
@@ -36,7 +36,7 @@ class TisAT0 < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/tis-system/artifacts/releases/download/nightly/cli_nightly_linux_arm64.tar.gz"
-      sha256 "31b20e36dcee2ea6ccf0f0c6faf6026994e17ae3585bb297781e9098fcd07222"
+      sha256 "1f02d9329bb67eb1a7b717976d3ff1ad9031b24be6c3ef49b49ca4d12a22b0e3"
 
       def install
         bin.install "tis"
