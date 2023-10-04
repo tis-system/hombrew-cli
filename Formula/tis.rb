0bb638defd1ddb23490ec216a25bf5b1919db787
tetrate-ci <52504619+tetrate-ci@users.noreply.github.com>
2023-10-04 04:01:30 +0000

    Apply automatic changes

--- Formula/tis.rb
+++ Formula/tis.rb
@@ -2,22 +2,22 @@
 # frozen_string_literal: true
 
 class Tis < Formula
-  desc "A CLI for TIS API (v0.0.1-rc2)"
+  desc "A CLI for TIS API (v0.0.1-rc0)"
   homepage "https://tis.tetrate.io"
-  version "v0.0.1-rc2" # v0.0.1-rc2
+  version "v0.0.1-rc0" # v0.0.1-rc0
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://tis.tetrate.io/artifacts/v0.0.1-rc2/cli_v0.0.1-rc2_darwin_amd64.tar.gz"
-      sha256 "82cf0d28ee17bc9497bdf1ed9024db234dc881f934c5e2b63c5a72c44c8f4fda"
+      url "https://tis.tetrate.io/artifacts/v0.0.1-rc0/cli_v0.0.1-rc0_darwin_amd64.tar.gz"
+      sha256 "9d55df76a8b6d8b39942ffb03fce55e1167cb1be22031ca48b97c098de071b4c"
 
       def install
         bin.install "tis"
       end
     end
     if Hardware::CPU.arm?
-      url "https://tis.tetrate.io/artifacts/v0.0.1-rc2/cli_v0.0.1-rc2_darwin_arm64.tar.gz"
-      sha256 "b07b87a2174f4fa9dce6b72c55948e7311235f06fac03577c8037399f78cb631"
+      url "https://tis.tetrate.io/artifacts/v0.0.1-rc0/cli_v0.0.1-rc0_darwin_arm64.tar.gz"
+      sha256 "10a271e39600e4bce6a7da9aa41e79d4284a0b91ce8150b399cf92b00bb57641"
 
       def install
         bin.install "tis"
@@ -27,16 +27,16 @@ class Tis < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://tis.tetrate.io/artifacts/v0.0.1-rc2/cli_v0.0.1-rc2_linux_amd64.tar.gz"
-      sha256 "ff475020a0e3b910083d55de7f4fd3337e9cc3a69bf7fac0d8a8c841772e0a01"
+      url "https://tis.tetrate.io/artifacts/v0.0.1-rc0/cli_v0.0.1-rc0_linux_amd64.tar.gz"
+      sha256 "6d8de80a38427311b6846f77c5d4cdb48fb6d240ac619a75b25a48bad9da5872"
 
       def install
         bin.install "tis"
       end
     end
     if Hardware::CPU.intel?
-      url "https://tis.tetrate.io/artifacts/v0.0.1-rc2/cli_v0.0.1-rc2_linux_arm64.tar.gz"
-      sha256 "a1c8dc6c3ab51bd04ee5c06dc5fe472b8c886c885c659333e6b0cee95d280d8a"
+      url "https://tis.tetrate.io/artifacts/v0.0.1-rc0/cli_v0.0.1-rc0_linux_arm64.tar.gz"
+      sha256 "91990a736555b5a49113d2833e2c23392dc9f6fbc3b6ef1c996fcc363b17ee4f"
 
       def install
         bin.install "tis"
